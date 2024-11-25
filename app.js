2998da95fe754ee081d6d5fc01f9d64e8aa5565d
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2024-11-25 11:03:01 -0300

    aula 2

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroAleatorio = gerarNumeroAleatorio();
+console.log(numeroAleatorio);
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10.';
+function exibirTextoTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoTela('h1', 'Jogo do número secreto');
+exibirTextoTela('p', 'Escolha um número entre 1 e 10.');
 
 function verificarChute(){
-    
+    let chute = document.querySelector('input').value;
+    if(chute == numeroAleatorio){
+        exibirTextoTela('p', 'Acertou.');
+    }
+    console.log(numeroAleatorio)
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
 }
